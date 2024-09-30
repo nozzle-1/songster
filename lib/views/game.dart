0b24fd6fe3eded70594ec7453920b0ea83a9e82e
Clémentine D <debailleux.clementine@gmail.com>
2024-09-30 18:28:29 +0200

    wip: ui with buttons + disabled when scanning

--- lib/views/game.dart
+++ lib/views/game.dart
@@ -17,7 +17,7 @@ class Game extends StatefulWidget {
 
 class _GameState extends State<Game> with TickerProviderStateMixin {
   final flipController = FlipCardController();
-  bool _isScanning = false;
+  bool _isScanning = true;
   bool _isPlaying = false;
   bool _isDownloading = false;
   final HitsterSongPlayer _player = JustAudioSongPlayer();
@@ -83,102 +83,98 @@ class _GameState extends State<Game> with TickerProviderStateMixin {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Center(
-              child: AspectRatio(
-                aspectRatio: 1,
-                child: Builder(builder: (context) {
-                  if (_isScanning) {
-                    return Card(
-                      margin: const EdgeInsets.all(25),
-                      child: ClipRRect(
-                          borderRadius: BorderRadius.circular(12.0),
-                          child: HitsterCardScanner(
-                            onDetect: (hitsterSongUrl) async =>
-                                await onDetectSong(hitsterSongUrl),
-                          )),
-                    );
-                  }
-                  if (_showCard) {
-                    return HitsterCard(hitsterUrl: _songUrl!.url, song: _song);
-                  }
-                  return const SizedBox();
-                }),
-              ),
-            ),
-            if (_showCard)
-              Row(
+            Expanded(
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  IconButton.filled(
-                    onPressed: () async {
-                      await _player.backward();
-                    },
-                    icon: Builder(builder: (context) {
-                      return const Icon(Icons.replay_10);
-                    }),
-                    style: ElevatedButton.styleFrom(
-                      shape: const CircleBorder(),
-                      backgroundColor: Colors.white.withAlpha(70),
-                    ),
-                  ),
-                  IconButton.filled(
-                    onPressed: () async => await pressMusicStatusButton(),
-                    icon: Builder(builder: (context) {
-                      if (_isDownloading) {
-                        return const CircularProgressIndicator(
-                          color: Colors.white,
+                  AspectRatio(
+                    aspectRatio: 1,
+                    child: Builder(builder: (context) {
+                      if (_isScanning) {
+                        return Card(
+                          margin: const EdgeInsets.all(25),
+                          child: ClipRRect(
+                              borderRadius: BorderRadius.circular(12.0),
+                              child: HitsterCardScanner(
+                                onDetect: (hitsterSongUrl) async =>
+                                    await onDetectSong(hitsterSongUrl),
+                              )),
                         );
                       }
-                      return Icon(_isPlaying ? Icons.stop : Icons.play_arrow);
+                      if (_showCard) {
+                        return HitsterCard(
+                            hitsterUrl: _songUrl!.url, song: _song);
+                      }
+                      return const SizedBox();
                     }),
-                    style: ElevatedButton.styleFrom(
-                      shape: const CircleBorder(),
-                      backgroundColor: Colors.white.withAlpha(70),
-                    ),
                   ),
-                  IconButton.filled(
-                    onPressed: () async {
-                      await _player.forward();
-                    },
-                    icon: Builder(builder: (context) {
-                      return const Icon(Icons.forward_10);
-                    }),
-                    style: ElevatedButton.styleFrom(
-                      shape: const CircleBorder(),
-                      backgroundColor: Colors.white.withAlpha(70),
-                    ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 32, vertical: 16),
+                    child: StreamBuilder<Duration>(
+                        stream: _player.currentPosition,
+                        builder: (context, snapshot) {
+                          final songDuration = _player.duration;
+                          final currentPosition =
+                              snapshot.data ?? const Duration();
+                          final percentage = songDuration.inSeconds <= 0
+                              ? 0
+                              : (currentPosition.inSeconds /
+                                      songDuration.inSeconds) *
+                                  100;
+
+                          return TweenAnimationBuilder<double>(
+                            duration: const Duration(milliseconds: 500),
+                            curve: Curves.easeInOut,
+                            tween: Tween<double>(
+                              begin: 0,
+                              end: percentage / 100,
+                            ),
+                            builder: (context, value, _) =>
+                                LinearProgressIndicator(
+                                    borderRadius: BorderRadius.circular(25),
+                                    minHeight: 10,
+                                    backgroundColor: _isScanning
+                                        ? const Color.fromRGBO(72, 30, 138,
+                                            70) // TODO find disabled color
+                                        : Colors.white.withAlpha(70),
+                                    color: Colors.white,
+                                    value: _isScanning ? 0 : value),
+                          );
+                        }),
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      RoundIconButton(
+                        icon: Icons.replay_10,
+                        onPressed: _isScanning
+                            ? null
+                            : () async => await _player.backward(),
+                      ),
+                      RoundIconButton(
+                        icon: _isPlaying ? Icons.pause : Icons.play_arrow,
+                        onPressed: _isScanning
+                            ? null
+                            : () async => await pressMusicStatusButton(),
+                        size: ButtonSize.big,
+                        isLoading: !_isScanning && _isDownloading,
+                      ),
+                      RoundIconButton(
+                        icon: Icons.forward_10,
+                        onPressed: _isScanning
+                            ? null
+                            : () async => await _player.forward(),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            StreamBuilder<Duration>(
-                stream: _player.currentPosition,
-                builder: (context, snapshot) {
-                  final songDuration = _player.duration;
-                  final currentPosition = snapshot.data ?? const Duration();
-                  final percentage = songDuration.inSeconds <= 0
-                      ? 0
-                      : (currentPosition.inSeconds / songDuration.inSeconds) *
-                          100;
-
-                  return TweenAnimationBuilder<double>(
-                    duration: const Duration(milliseconds: 500),
-                    curve: Curves.easeInOut,
-                    tween: Tween<double>(
-                      begin: 0,
-                      end: percentage / 100,
-                    ),
-                    builder: (context, value, _) => LinearProgressIndicator(
-                        backgroundColor: Colors.white.withAlpha(70),
-                        color: Colors.white,
-                        value: value),
-                  );
-                }),
-            Align(
-              alignment: AlignmentDirectional.bottomCenter,
-              child: MainButton(
-                label: "Scanner",
-                onPressed: () async => await launchScan(),
-              ),
+            ),
+            MainButton(
+              label: _showCard ? "Suivant" : "Scan...",
+              onPressed: _isScanning ? null : () async => await launchScan(),
             ),
           ],
         ),
--- lib/widgets/buttons.dart
+++ lib/widgets/buttons.dart
@@ -34,3 +34,45 @@ class MainButton extends StatelessWidget {
     );
   }
 }
+
+class RoundIconButton extends StatelessWidget {
+  const RoundIconButton({
+    super.key,
+    this.onPressed,
+    required this.icon,
+    this.size = ButtonSize.small,
+    this.isLoading = false,
+  });
+  final void Function()? onPressed;
+  final IconData icon;
+  final ButtonSize size;
+  final bool isLoading;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: IconButton.filled(
+        iconSize: size == ButtonSize.big ? 48 : 24,
+        onPressed: onPressed,
+        icon: Builder(builder: (context) {
+          if (isLoading) {
+            return const CircularProgressIndicator(
+              color: Colors.white,
+            );
+          }
+          return Icon(
+            icon,
+          );
+        }),
+        style: ElevatedButton.styleFrom(
+          fixedSize: Size.square(size == ButtonSize.big ? 64 : 48),
+          shape: const CircleBorder(),
+          backgroundColor: Colors.white.withAlpha(70),
+        ),
+      ),
+    );
+  }
+}
+
+enum ButtonSize { small, big }
